2f53b0a93c8b10ca606285a5e80b519429f14b7d
Ueliton Almeida <uealmeida@gmail.com>
2022-07-16 18:51:55 -0300

    ...

---
+++ Secao_06/App01_LayoutXF/App01_LayoutXF/App01_LayoutXF/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_LayoutXF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new App01_LayoutXF.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
